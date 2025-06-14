a79ff52c865ee60483f21594df3571f03da16ecb
BaoNgo208 <128871855+BaoNgo208@users.noreply.github.com>
2025-06-14 14:48:03 +0700

    update yml

--- src/main/java/com/example/spring_boot_react_demo/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_react_demo/config/SecurityConfig.java
@@ -18,7 +18,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://doantotnghiepbackend-production.up.railway.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/example/spring_boot_react_demo/util/Constants.java
+++ src/main/java/com/example/spring_boot_react_demo/util/Constants.java
@@ -15,8 +15,7 @@ public final class Constants {
     public static final int ONE = 1;
     public static final String API_URL = "https://api.openai.com/v1/audio/transcriptions";
     public static final MediaType MEDIA_TYPE_AUDIO = MediaType.parse("audio/mpeg");
-    public static final String OPENAI_API_KEY = "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";
-
+    public static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
     public static final String HEADER_FILE_PATH = "src/main/java/com/example/spring_boot_react_demo/util/ass_header.txt";
     public static final String NEW_LINE = "\n";
     public static final String COMMA = ",";
