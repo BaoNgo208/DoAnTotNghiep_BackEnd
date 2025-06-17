8287c46eb6566a4bf870646b880741cd52c76d01
BaoNgo208 <128871855+BaoNgo208@users.noreply.github.com>
2025-06-17 11:13:25 +0700

    updated cloudname

--- src/main/java/com/example/spring_boot_react_demo/util/Constants.java
+++ src/main/java/com/example/spring_boot_react_demo/util/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
     }
 
     public static final String DOT= ".";
-    public static final String CLOUDINARY_UPLOAD_URL = "https://res.cloudinary.com/ds7sx6zop/";
+    public static final String CLOUDINARY_UPLOAD_URL = "https://res.cloudinary.com/dlqmbuvb0/";
     public static final String LOCAL_UPLOAD_URL = "/upload/v1/";
     public static final String OVERLAY_PREFIX = "l_";
     public static final int NOT_FOUND = -1;
